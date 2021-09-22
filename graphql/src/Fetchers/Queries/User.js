2ef8b480bde8b58b0e896b7f090003baee6f5ba1
Chanda Mulenga <dev@stctheproducer.com>
2021-09-22 02:43:26 +0200

    feat: Drop GraphQL container library

--- graphql/src/Fetchers/Queries/User.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  Query: {
-    user: async (_, { id }, context) => await context.dataSources.userAPI.findUserById(id),
-    userEmail: async (_, { email }, context) => await context.dataSources.userAPI.findUserByEmail(email),
-    me: async (_, __, context) => await context.dataSources.userAPI.sessionUser,
-  },
-}
--- graphql/src/Middleware/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  isAuthenticated: require('./isAuthenticated'),
-}
